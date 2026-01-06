03df73902fe9e80b060bad221d8d34b15205d8a5
Rupesh <50985060+rupeshnadar@users.noreply.github.com>
2026-01-06 19:54:18 +0530

    Create atm_simulator.py

---
+++ atm_simulator.py
@@ -0,0 +1,16 @@
+correct_pin = 1234
+balance = 100000
+
+pin = int(input("Enter ATM Pin: "))
+
+if pin == correct_pin:
+    withdr = int(input("Amount to Withdraw: "))
+
+    if withdr <= balance:
+        balance = balance - withdr
+        print("Collect Money ....")
+        print(f"Remaining Balance: {balance}")
+    else:
+        print("Insufficient Balance")
+else:
+    print("Access Denied, Invalid PIN")
